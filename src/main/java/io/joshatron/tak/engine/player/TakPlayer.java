6a3a2801350eb573b846c3f38331befef84445dc
Joshua Leger <legermjoshua@gmail.com>
2019-01-20 13:14:35 -0800

    adding exception

--- src/main/java/io/joshatron/tak/engine/player/TakPlayer.java
+++ src/main/java/io/joshatron/tak/engine/player/TakPlayer.java
@@ -1,9 +1,10 @@
 package io.joshatron.tak.engine.player;
 
+import io.joshatron.tak.engine.exception.TakEngineException;
 import io.joshatron.tak.engine.game.GameState;
 import io.joshatron.tak.engine.turn.Turn;
 
 public interface TakPlayer {
 
-    public Turn getTurn(GameState state);
+    public Turn getTurn(GameState state) throws TakEngineException;
 }
