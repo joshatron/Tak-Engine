a89b2db0d7cdaeee38b33551ce7901dc173ded0c
Joshua Leger <legermjoshua@gmail.com>
2019-06-15 11:15:55 -0700

    More work on engine

--- src/main/java/io/joshatron/tak/engine/engine/DiffNode.java
+++ src/main/java/io/joshatron/tak/engine/engine/DiffNode.java
@@ -2,16 +2,23 @@ package io.joshatron.tak.engine.engine;
 
 import io.joshatron.tak.engine.turn.TurnDiff;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
-import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
+@EqualsAndHashCode(exclude = {"children", "childrenFull"})
 public class DiffNode {
-    ArrayList<DiffNode> children;
-    TurnDiff diff;
+    private Set<DiffNode> children;
+    private TurnDiff diff;
+    private boolean childrenFull;
 
     public DiffNode(TurnDiff diff) {
         this.diff = diff;
-        children = new ArrayList<>();
+        children = new HashSet<>();
+        childrenFull = false;
     }
+
+
 }
--- src/main/java/io/joshatron/tak/engine/engine/TakEngine.java
+++ src/main/java/io/joshatron/tak/engine/engine/TakEngine.java
@@ -1,19 +1,55 @@
 package io.joshatron.tak.engine.engine;
 
+import io.joshatron.tak.engine.board.BoardLocation;
 import io.joshatron.tak.engine.exception.TakEngineException;
 import io.joshatron.tak.engine.game.GameState;
 import io.joshatron.tak.engine.game.Player;
 import io.joshatron.tak.engine.turn.Turn;
 import io.joshatron.tak.engine.turn.TurnDiff;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TakEngine {
 
     private GameState state;
     private DiffNode root;
+    private boolean restrictTurns;
 
     public TakEngine(Player first, int size) throws TakEngineException {
+        this(first, size, false);
+    }
+
+    public TakEngine(Player first, int size, boolean restrictTurns) throws TakEngineException {
         state = new GameState(first, size);
         root = new DiffNode(new TurnDiff());
+        this.restrictTurns = restrictTurns;
+    }
+
+    public List<Turn> getPossibleTurns() {
+        return getPossibleTurns(root);
+    }
+
+    private List<Turn> getPossibleTurns(DiffNode currentRoot) {
+        if(!currentRoot.isChildrenFull()) {
+            List<BoardLocation> locations = new ArrayList<>();
+
+            for(int x = 0; x < state.getBoardSize(); x++) {
+                for(int y = 0; y < state.getBoardSize(); y++) {
+                    locations.add(new BoardLocation(x, y));
+                }
+            }
+
+            locations.parallelStream().map(location -> getPossibleDiffsForLocation(location, currentRoot))
+                    .forEach(diffs -> currentRoot.getChildren().addAll(diffs));
+        }
+
+        //TODO: restrict if neccessary, then return
+        return null;
+    }
+
+    private List<DiffNode> getPossibleDiffsForLocation(BoardLocation location, DiffNode currentRoot) {
+        return null;
     }
 
     public void executeTurn(Turn turn) throws TakEngineException {
